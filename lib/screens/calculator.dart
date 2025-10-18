b19251c7700d6eff9176859534efcdae8be041f8
Ever-VC <ever.vasquezc27@gmail.com>
2025-10-18 12:27:09 -0600

    ✨ New Feature: Funciones básicas v1.00

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,6 +1,7 @@
 import 'package:calculadora/screens/widgets/calc_buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class CalculatorApp extends StatefulWidget {
   const CalculatorApp({super.key});
@@ -10,13 +11,94 @@ class CalculatorApp extends StatefulWidget {
 }
 
 class _CalculatorAppState extends State<CalculatorApp> {
-  final primaryColor = Color.fromARGB(255, 77, 77, 77);
-  final secondaryColor = Color.fromARGB(255, 143, 143, 143);
-  final baseColor = Color.fromARGB(0, 255, 82, 82);
-  double result = 0.0;
-  
+  final _primaryColor = Color.fromARGB(255, 77, 77, 77);
+  final _secondaryColor = Color.fromARGB(255, 143, 143, 143);
+  final _baseColor = Color.fromARGB(0, 255, 82, 82);
+  String _history = '';
+  String _experssion = '';
+  int startParentheses = 0;
+  int endParentheses = 0;
+
   void allClear(String text) {
-    setState(() {});
+    startParentheses = 0;
+    endParentheses = 0;
+    setState(() {
+      _history = '';
+      _experssion = '';
+    });
+  }
+
+  void clearOnce() {
+    setState(() {
+      // Verifica si el elemento a eliminar es un paréntesis
+      if (_experssion.isNotEmpty) {
+        String lastChar = _experssion[_experssion.length - 1];
+        if (lastChar == '(') {
+          startParentheses--;
+        } else if (lastChar == ')') {
+          endParentheses--;
+        }
+      }
+      _experssion = _experssion.substring(0, _experssion.length - 1);
+    });
+  }
+
+  void changeSing(String text) {
+    String tempExpress = _experssion;
+    String first = tempExpress.substring(0, _experssion.length - 1);
+    String lastest = tempExpress.substring(_experssion.length - 1);
+    tempExpress = "$first(-$lastest";
+    setState(() {
+      _experssion = tempExpress;
+    });
+  }
+
+  void addParentheses(String text) {
+    String temExpress = _experssion;
+    if (temExpress.isNotEmpty) {
+      String lastChar = temExpress[temExpress.length - 1];
+      if (double.tryParse(lastChar) == null && lastChar != ')') {
+        temExpress = "$temExpress(";
+        startParentheses++;
+      } else if (startParentheses < endParentheses ||
+          startParentheses == endParentheses) {
+        temExpress = "$temExpress×(";
+        startParentheses++;
+      } else {
+        temExpress = "$temExpress)";
+        endParentheses++;
+      }
+    } else {
+      temExpress = '(';
+      startParentheses++;
+    }
+
+    setState(() {
+      _experssion = temExpress;
+    });
+  }
+
+  void evaluate(String text) {
+    ExpressionParser parser = GrammarParser();
+    String tempExpress = _experssion.replaceAll('×', '*');
+    //tempExpress = tempExpress.replaceAll('(', '');
+    Expression expression = parser.parse(tempExpress);
+    ContextModel contextModel = ContextModel();
+    var evaluator = RealEvaluator(contextModel);
+    setState(() {
+      _history = _experssion;
+      num result = evaluator.evaluate(expression);
+      if (!(result is double && result != result.toInt())) {
+        result = result.round();
+      }
+      _experssion = result.toString();
+    });
+  }
+
+  void numberSelection(String text) {
+    setState(() {
+      _experssion += text;
+    });
   }
 
   @override
@@ -27,7 +109,7 @@ class _CalculatorAppState extends State<CalculatorApp> {
         child: Padding(
           padding: EdgeInsets.only(right: 12),
           child: Text(
-            "Historial",
+            _history,
             style: GoogleFonts.rubik(
               textStyle: const TextStyle(
                 fontSize: 24,
@@ -42,12 +124,35 @@ class _CalculatorAppState extends State<CalculatorApp> {
         child: Padding(
           padding: EdgeInsets.all(12),
           child: Text(
-            "2+8",
+            _experssion,
             style: GoogleFonts.rubik(
               textStyle: const TextStyle(fontSize: 48, color: Colors.white),
             ),
           ),
         ),
+      ),
+      Container(
+        alignment: Alignment(1.0, 1.0),
+        child: Padding(
+          padding: EdgeInsets.all(12),
+          child: ElevatedButton(
+            onPressed: () {
+              clearOnce();
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 15, 20, 20),
+              foregroundColor: Colors.green,
+            ),
+            child: Icon(Icons.backspace),
+          ),
+        ),
+      ),
+      Divider(
+        color: Colors.grey, // Color of the line
+        height: 20, // Total height occupied by the divider (including padding)
+        thickness: 1, // Thickness of the line itself
+        indent: 16, // Indent from the start of the line
+        endIndent: 16, // Indent from the end of the line
       ),
       Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -56,25 +161,25 @@ class _CalculatorAppState extends State<CalculatorApp> {
             callback: allClear,
             text: "AC",
             textSize: 20,
-            color: primaryColor,
+            color: _primaryColor,
           ),
           CalcButton(
-            callback: allClear,
-            text: "C",
+            callback: addParentheses,
+            text: "()",
             textSize: 20,
-            color: primaryColor,
+            color: _primaryColor,
           ),
           CalcButton(
-            callback: allClear,
+            callback: numberSelection,
             text: "%",
             textSize: 20,
-            color: primaryColor,
+            color: _primaryColor,
           ),
           CalcButton(
-            callback: allClear,
+            callback: numberSelection,
             text: "/",
             textSize: 20,
-            color: secondaryColor,
+            color: _secondaryColor,
           ),
         ],
       ),
@@ -82,28 +187,28 @@ class _CalculatorAppState extends State<CalculatorApp> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           CalcButton(
-            callback: allClear,
+            callback: numberSelection,
             text: "7",
             textSize: 20,
-            color: baseColor,
+            color: _baseColor,
           ),
           CalcButton(
-            callback: allClear,
+            callback: numberSelection,
             text: "8",
             textSize: 20,
-            color: baseColor,
+            color: _baseColor,
           ),
           CalcButton(
-            callback: allClear,
+            callback: numberSelection,
             text: "9",
             textSize: 20,
-            color: baseColor,
+            color: _baseColor,
           ),
           CalcButton(
-            callback: allClear,
-            text: "X",
-            textSize: 20,
-            color: secondaryColor,
+            callback: numberSelection,
+            text: "×",
+            textSize: 28,
+            color: _secondaryColor,
           ),
         ],
       ),
@@ -111,28 +216,28 @@ class _CalculatorAppState extends State<CalculatorApp> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           CalcButton(
-            callback: allClear,
+            callback: numberSelection,
             text: "4",
             textSize: 20,
-            color: baseColor,
+            color: _baseColor,
           ),
           CalcButton(
-            callback: allClear,
+            callback: numberSelection,
             text: "5",
             textSize: 20,
-            color: baseColor,
+            color: _baseColor,
           ),
           CalcButton(
-            callback: allClear,
+            callback: numberSelection,
             text: "6",
             textSize: 20,
-            color: baseColor,
+            color: _baseColor,
           ),
           CalcButton(
-            callback: allClear,
+            callback: numberSelection,
             text: "-",
             textSize: 20,
-            color: secondaryColor,
+            color: _secondaryColor,
           ),
         ],
       ),
@@ -140,28 +245,28 @@ class _CalculatorAppState extends State<CalculatorApp> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           CalcButton(
-            callback: allClear,
+            callback: numberSelection,
             text: "1",
             textSize: 20,
-            color: baseColor,
+            color: _baseColor,
           ),
           CalcButton(
-            callback: allClear,
+            callback: numberSelection,
             text: "2",
             textSize: 20,
-            color: baseColor,
+            color: _baseColor,
           ),
           CalcButton(
-            callback: allClear,
+            callback: numberSelection,
             text: "3",
             textSize: 20,
-            color: baseColor,
+            color: _baseColor,
           ),
           CalcButton(
-            callback: allClear,
+            callback: numberSelection,
             text: "+",
             textSize: 20,
-            color: secondaryColor,
+            color: _secondaryColor,
           ),
         ],
       ),
@@ -169,25 +274,25 @@ class _CalculatorAppState extends State<CalculatorApp> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           CalcButton(
-            callback: allClear,
+            callback: changeSing,
             text: "+/-",
             textSize: 20,
-            color: baseColor,
+            color: _baseColor,
           ),
           CalcButton(
-            callback: allClear,
+            callback: numberSelection,
             text: "0",
             textSize: 20,
-            color: baseColor,
+            color: _baseColor,
           ),
           CalcButton(
-            callback: allClear,
+            callback: numberSelection,
             text: ".",
             textSize: 20,
-            color: baseColor,
+            color: _baseColor,
           ),
           CalcButton(
-            callback: allClear,
+            callback: evaluate,
             text: "=",
             textSize: 20,
             color: const Color.fromARGB(255, 0, 117, 6),
